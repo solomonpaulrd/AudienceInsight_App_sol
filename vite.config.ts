f91818198c7e26c62a13a0be8171b8b93da175e0
solomonpaulrd <47301906+solomonpaulrd@users.noreply.github.com>
2025-05-16 11:45:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/AudienceInsight_App_sol/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
-
